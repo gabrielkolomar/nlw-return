548683948f306392acd1d9077f7b3f1ff0a0e126
Gabriel Kolomar <gkpequenino@gmail.com>
2022-05-08 21:14:04 -0300

    final commit

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2 // Linha alvo
+
+  // Verificar se a sessão passou da linha
+  // Quais dados vou precisar?
+
+  const sectionTop = section.offsetTop // O topo da seção
+  const sectionHeight = section.offsetHeight // A altura da seção
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop // O topo da seção chegou ou ultrapassou a linha alvo
+
+  //Verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight // A secão termina onde?
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine // O final da seção passou da linha
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine // Limites da seção passou da linha
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
